49a0875a81ca2882e273821f090f1871fd6432f7
Shun Li <shun.li.at.casia@outlook.com>
2024-06-18 20:47:32 +0800

    update

---
+++ src/tool/rosbag_imu_modifer.cc
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ *   Copyright (C) 2023 CASIA. All rights reserved.
+ *
+ *   @Filename: rosbag_imu_modifer.cc
+ *
+ *   @Author: shun li
+ *
+ *   @Email: shun.li.at.casia@outlook.com
+ *
+ *   @Date: 18/06/2024
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#include "utility_tool/print_ctrl_macro.h"
+#include "utility_tool/pcm_debug_helper.h"
+#include "utility_tool/cmdline.h"
+
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <ros/time.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <boost/foreach.hpp>
+
+void adjustImuAccelerationAndSave(const std::string& input_bag_path,
+                                  const std::string& output_bag_path,
+                                  const std::string& imu_topic,
+                                  double adjustment_factor) {
+  // 打开源bag文件
+  rosbag::Bag in_bag(input_bag_path, rosbag::bagmode::Read);
+
+  // 创建目标bag文件
+  rosbag::Bag out_bag(output_bag_path, rosbag::bagmode::Write);
+
+  // 获取源bag中的imu数据
+  std::vector<std::string> topics;
+  topics.push_back(imu_topic);  // 假设imu数据发布在"/imu/data"话题上
+  rosbag::View view(in_bag, rosbag::TopicQuery(topics));
+
+  // 遍历bag中的每一条imu消息
+  BOOST_FOREACH (rosbag::MessageInstance const m, view) {
+    sensor_msgs::Imu::ConstPtr imu_msg = m.instantiate<sensor_msgs::Imu>();
+    if (imu_msg != nullptr) {
+      // 创建一个新的Imu消息实例，用于存储修改后的数据
+      sensor_msgs::Imu modified_imu_msg(*imu_msg);
+
+      // 修改加速度值（乘以一个因子作为示例）
+      modified_imu_msg.linear_acceleration.x *= adjustment_factor;
+      modified_imu_msg.linear_acceleration.y *= adjustment_factor;
+      modified_imu_msg.linear_acceleration.z *= adjustment_factor;
+
+      // 写入新bag，使用原始topic名称
+      out_bag.write(imu_topic, m.getTime(), modified_imu_msg);
+    }
+  }
+
+  // 关闭bag文件
+  in_bag.close();
+  out_bag.close();
+
+  PCM_STREAM_INFO("IMU acceleration adjustment completed and saved to: "
+                  << output_bag_path << std::endl);
+}
+
+int main(int argc, char** argv) {
+  cmdline::parser par;
+  par.add<std::string>("input_bag_path", 'i', "input bag path", true);
+  par.add<std::string>("output_bag_path", 'o', "output bag path", true);
+  par.add<std::string>("imu_topic", 't', "imu topic", true);
+  par.add<double>("adjustment_factor", 'f', "adjustment factor", true);
+  par.parse_check(argc, argv);
+
+  std::string input_bag_path = par.get<std::string>("input_bag_path");
+  std::string output_bag_path = par.get<std::string>("output_bag_path");
+  std::string imu_topic = par.get<std::string>("imu_topic");
+  double adjustment_factor = par.get<double>("adjustment_factor");
+
+  adjustImuAccelerationAndSave(input_bag_path, output_bag_path, imu_topic,
+                               adjustment_factor);
+
+  return 0;
+}
